709a2dc3d619f20f68e7ca246d352b2dbd0a1b69
Yuzu-Lemon0822 <yuzulemon140416@gmail.com>
2026-01-19 22:18:57 +0900

    Update input.js

--- game/input.js
+++ game/input.js
@@ -1,1 +1,21 @@
+export const input = {
+  w: false,
+  a: false,
+  s: false,
+  d: false
+};
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "w") input.w = true;
+  if (e.key === "a") input.a = true;
+  if (e.key === "s") input.s = true;
+  if (e.key === "d") input.d = true;
+});
+
+window.addEventListener("keyup", (e) => {
+  if (e.key === "w") input.w = false;
+  if (e.key === "a") input.a = false;
+  if (e.key === "s") input.s = false;
+  if (e.key === "d") input.d = false;
+});
 
