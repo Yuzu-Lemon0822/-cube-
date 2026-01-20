095da12742ab2c1dab5fa5f15f6e09763385a2e0
Yuzu-Lemon0822 <yuzulemon140416@gmail.com>
2026-01-20 15:15:20 +0900

    Update display.js

--- game/display.js
+++ game/display.js
@@ -3,6 +3,7 @@ import { player } from "../data/player.js";
 let ctx;
 let canvas;
 let playerImg;
+let textureList = {} //テクスチャデータをキャッシュできるならやりたい。
 
 export function initDisplay() {
   canvas = document.getElementById("game");
@@ -12,19 +13,19 @@ export function initDisplay() {
 
   canvas.width = 800;
   canvas.height = 600;
+}
 
-  playerImg = new Image();
-  playerImg.src = "texture/player.png";
+function display(x, y, w, h, texture) {
+  let displayImage = new Image()
+  displayImage.src = "texture/" + texture + ".png"
+
+  ctx.drawImage(displayImage, x, y, w, h)
 }
 
 export function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.drawImage(
-    playerImg,
-    player.x,
-    player.y,
-    player.w,
-    player.h
-  );
+  ctx.display("player", player.x, player.y, player.w, player.h );
+
+  ctx.display("stage",200,200,64,64)
 }
