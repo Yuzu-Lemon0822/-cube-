0b0d1bcf345b4a4225c7fe403b21c68e3d562634
Yuzu-Lemon0822 <yuzulemon140416@gmail.com>
2026-01-20 15:40:49 +0900

    Update process.js

--- game/process.js
+++ game/process.js
@@ -5,6 +5,7 @@ export function update() {
   if (input.d) player.powerX += player.speed;
   if (input.a) player.powerX -= player.speed;
   player.powerX *= 0.9;
+  player.x += player.powerX;
 
   // 画面外に出ないようにする（仮）
   player.x = Math.max(0, Math.min(800 - player.w, player.x));
